0d0332506f86dfc798574b8b47c526eca485f356
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2025-02-28 16:12:59 +0700

    up laprak

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/SourceCode/praktikum1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace praktikum1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNamakuSiapa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNamakuSiapa
+            // 
+            this.btnNamakuSiapa.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNamakuSiapa.Location = new System.Drawing.Point(279, 203);
+            this.btnNamakuSiapa.Name = "btnNamakuSiapa";
+            this.btnNamakuSiapa.Size = new System.Drawing.Size(619, 401);
+            this.btnNamakuSiapa.TabIndex = 0;
+            this.btnNamakuSiapa.Text = "NAMAKU SIAPA?";
+            this.btnNamakuSiapa.UseVisualStyleBackColor = true;
+            this.btnNamakuSiapa.Click += new System.EventHandler(this.btnNamakuSiapa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 858);
+            this.Controls.Add(this.btnNamakuSiapa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNamakuSiapa;
+    }
+}
+
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/SourceCode/praktikum1_console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikum1_console
+{
+    class Program
+    {
+        enum State { START, GAME, PAUSE, EXIT };
+        public static void Main()
+        {
+            State state = State.START;
+            string[] screenName = { "START", "GAME", "PAUSE", "EXIT" };
+            while (state != State.EXIT)
+            {
+                Console.WriteLine(screenName[(int)state] + " SCREEN");
+                Console.Write("Enter Command : ");
+                string command = Console.ReadLine();
+                switch (state)
+                {
+                    case State.START:
+                        if (command == "ENTER")
+                            state = State.GAME;
+                        else if (command == "QUIT")
+                            state = State.EXIT;
+                        else
+                            state = State.START;
+                        break;
+                    case State.GAME:
+                        if (command == "ESC")
+                            state = State.PAUSE;
+                        else
+                            state = State.GAME;
+                        break;
+                    case State.PAUSE:
+                        if (command == "BACK")
+                            state = State.GAME;
+                        else if (command == "HOME")
+                            state = State.START;
+                        else if (command == "QUIT")
+                            state = State.EXIT;
+                        else
+                            state = State.PAUSE;
+                        break;
+                }
+            }
+            Console.WriteLine("EXIT SCREEN");
+        }
+    }
+}
